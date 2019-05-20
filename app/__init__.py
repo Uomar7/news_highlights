e0549d92f1fe206dfde5457a2e7aad912953d09f
Uomar <uomarearlie7@gmail.com>
2019-05-20 12:03:44 +0300

    initiatiated the confi settings in the init.

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from .config import DevConfig
 
 #initializing the application
-app = Flask(__name__)
+app = Flask(__name__,instance_relative_config=True)
 
 # setting up the configurations
 app.config.from_object(DevConfig)
+app.config.from_object('config.py')
 
 from app import views
