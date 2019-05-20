6b5f92fb7b6a6d85a4ba8e22ed335e21c9d8942e
Uomar <uomarearlie7@gmail.com>
2019-05-20 15:55:14 +0300

    display the different news sources

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,12 @@ from .config import DevConfig
 
 #initializing the application
 app = Flask(__name__,instance_relative_config=True)
+from flask_bootstrap import Bootstrap
 
 # setting up the configurations
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+
+bootstrap = Bootstrap(app)
 
 from app import views
