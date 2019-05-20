f347ec0963451690240abf1a3313ecd8c75be39d
Uomar <uomarearlie7@gmail.com>
2019-05-20 12:49:45 +0300

    imported the request module in the views module

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_sources
 
 #views
 @app.route('/')
@@ -7,8 +8,13 @@ def index():
     '''
     view root page function that returns the index page and its page
     '''
+
+    # getting newa articles from different sources.
+    business_news = get_sources('business')
+    print(business_news)
+
     title = 'Home - Welcome to the Best News Online Site.'
-    return render_template('index.html')
+    return render_template('index.html', title=title, business=business_news)
 
 @app.route('/source/<source>')
 def source(source):
